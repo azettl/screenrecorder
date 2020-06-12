0df5a0f2bbdea30811b1e8e874a04e625c7ae65c
Andreas <info@azettl.net>
2020-06-12 23:44:47 +0200

    add download link

--- assets/js/recorder.js
+++ assets/js/recorder.js
@@ -2,7 +2,8 @@ const videoElem = document.getElementById("video");
 const logElem = document.getElementById("log");
 const startElem = document.getElementById("start");
 const stopElem = document.getElementById("stop");
-
+var chunks = [];
+var recording = null;
 // Options for getDisplayMedia()
 
 var displayMediaOptions = {
@@ -22,21 +23,41 @@ stopElem.addEventListener("click", function(evt) {
 }, false); console.log = msg => logElem.innerHTML += `${msg}<br>`;
 console.error = msg => logElem.innerHTML += `<span class="error">${msg}</span><br>`;
 console.warn = msg => logElem.innerHTML += `<span class="warn">${msg}<span><br>`;
-console.info = msg => logElem.innerHTML += `<span class="info">${msg}</span><br>`; async function startCapture() {
+console.info = msg => logElem.innerHTML += `<span class="info">${msg}</span><br>`; 
+
+async function startCapture() {
   logElem.innerHTML = "";
 
   try {
     videoElem.srcObject = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+
+    mediaRecorder = new MediaRecorder(videoElem.srcObject, {mimeType: 'video/webm'});
+    mediaRecorder.addEventListener('dataavailable', event => {
+      if (event.data && event.data.size > 0) {
+        chunks.push(event.data);
+      }
+    });
+    mediaRecorder.start(10);
+
     dumpOptionsInfo();
   } catch(err) {
     console.error("Error: " + err);
   }
-} function stopCapture(evt) {
+} 
+
+function stopCapture(evt) {
   let tracks = videoElem.srcObject.getTracks();
 
   tracks.forEach(track => track.stop());
+
+  recording = window.URL.createObjectURL(new Blob(this.chunks, {type: 'video/webm'}));
+
+    document.getElementById("resultLink").href = recording;
+
   videoElem.srcObject = null;
-} function dumpOptionsInfo() {
+} 
+
+function dumpOptionsInfo() {
   const videoTrack = videoElem.srcObject.getVideoTracks()[0];
  
   console.info("Track settings:");
