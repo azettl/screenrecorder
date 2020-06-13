0a68bae2e51eb2ee0337c1db765ee0b55b60de69
Andreas <info@azettl.net>
2020-06-13 10:43:47 +0200

    Update recorder.js

--- assets/js/recorder.js
+++ assets/js/recorder.js
@@ -59,8 +59,8 @@ async function startCapture() {
 function stopCapture(evt) {
   let tracks = videoElem.srcObject.getTracks();
 
-  tracks.forEach(track => console.log(track));
   tracks.forEach(track => track.stop());
+  chunks.forEach(chunk => console.log(chunk));
   
   recordingMP4 = window.URL.createObjectURL(new Blob(chunks, {type: 'video/mp4'}));
   recordingWEBM = window.URL.createObjectURL(new Blob(chunks, {type: 'video/webm'}));
