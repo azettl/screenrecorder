6fff4508b783ca4dfaf12aa04b781085fbe50732
Andreas <info@azettl.net>
2020-06-17 20:31:15 +0200

    Update recorder.js

--- assets/js/recorder.js
+++ assets/js/recorder.js
@@ -202,7 +202,11 @@
 
                             var width  = (videoElem.videoWidth / 100) * 20;
                             var height = (videoElem.videoHeight / 100) * 20;
-                            // You can do whatever you want with this canvas context
+                            
+                            ctx.shadowOffsetX = 0;
+                            ctx.shadowOffsetY = 0;
+                            ctx.shadowBlur = 20;
+                            ctx.shadowColor = '#6ac2b6';
                             ctx.fillStyle = "#6ac2b6";
                             ctx.fillRect(x -5, y - 5, width + 10, height + 10);
                             ctx.drawImage(frame, x, y, width, height);
